ab62991353b1113a7120d6596f4a7288939ea096
junglator <srivastavaadi17@gmail.com>
2022-12-26 21:23:16 +0530

    updates some files

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -1,128 +1,38 @@
 import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
-from launch.conditions import IfCondition, UnlessCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, LaunchConfiguration, PythonExpression
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
- 
- 
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
 def generate_launch_description():
- 
-  # Constants for paths to different files and folders
-  gazebo_models_path = 'models'
-  package_name = 'gypsy_robot'
-  robot_name_in_model = 'gypsy'
-  sdf_model_path = 'models/gypsy.sdf'
-  world_file_path = 'worlds/supermarket.sdf'
-     
-  # Pose where we want to spawn the robot
-  spawn_x_val = '0.0'
-  spawn_y_val = '0.0'
-  spawn_z_val = '0.0'
-  spawn_yaw_val = '0.0'
- 
-  ############ You do not need to change anything below this line #############
-   
-  # Set the path to different files and folders.  
-  pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')   
-  pkg_share = FindPackageShare(package=package_name).find(package_name)
-  world_path = os.path.join(pkg_share, world_file_path)
-  gazebo_models_path = os.path.join(pkg_share, gazebo_models_path)
-  os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
-  sdf_model_path = os.path.join(pkg_share, sdf_model_path)
-   
-  # Launch configuration variables specific to simulation
-  gui = LaunchConfiguration('gui')
-  headless = LaunchConfiguration('headless')
-  namespace = LaunchConfiguration('namespace')
-  sdf_model = LaunchConfiguration('sdf_model')
-  use_namespace = LaunchConfiguration('use_namespace')
-  use_sim_time = LaunchConfiguration('use_sim_time')
-  use_simulator = LaunchConfiguration('use_simulator')
-  world = LaunchConfiguration('world')
-   
-  # Declare the launch arguments  
-  declare_namespace_cmd = DeclareLaunchArgument(
-    name='namespace',
-    default_value='',
-    description='Top-level namespace')
- 
-  declare_use_namespace_cmd = DeclareLaunchArgument(
-    name='use_namespace',
-    default_value='False',
-    description='Whether to apply a namespace to the navigation stack')
-             
-  declare_sdf_model_path_cmd = DeclareLaunchArgument(
-    name='sdf_model', 
-    default_value=sdf_model_path, 
-    description='Absolute path to robot sdf file')
- 
-  declare_simulator_cmd = DeclareLaunchArgument(
-    name='headless',
-    default_value='False',
-    description='Whether to execute gzclient')
-     
-  declare_use_sim_time_cmd = DeclareLaunchArgument(
-    name='use_sim_time',
-    default_value='True',
-    description='Use simulation (Gazebo) clock if true')
- 
-  declare_use_simulator_cmd = DeclareLaunchArgument(
-    name='use_simulator',
-    default_value='True',
-    description='Whether to start the simulator')
- 
-  declare_world_cmd = DeclareLaunchArgument(
-    name='world',
-    default_value=world_path,
-    description='Full path to the world model file to load')
-   
-  # Start Gazebo server
-  start_gazebo_server_cmd = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
-    condition=IfCondition(use_simulator),
-    launch_arguments={'world': world}.items())
- 
-  # Start Gazebo client    
-  start_gazebo_client_cmd = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')),
-    condition=IfCondition(PythonExpression([use_simulator, ' and not ', headless])))
- 
-  # Launch the robot
-  spawn_entity_cmd = Node(
-    package='gazebo_ros', 
-    executable='spawn_entity.py',
-    arguments=['-entity', robot_name_in_model, 
-               '-file', sdf_model,
-                  '-x', spawn_x_val,
-                  '-y', spawn_y_val,
-                  '-z', spawn_z_val,
-                  '-Y', spawn_yaw_val],
-                  output='screen')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    robot_name = 'gypsy_robot'
+    world_file_name = 'supermarket.sdf'
 
-  controller_node = Node(
-    package=package_name,
-    executable="controller.yaml"
-    )
- 
-  # Create the launch description and populate
-  ld = LaunchDescription()
- 
-  # Declare the launch options
-  ld.add_action(declare_namespace_cmd)
-  ld.add_action(declare_use_namespace_cmd)
-  ld.add_action(declare_sdf_model_path_cmd)
-  ld.add_action(declare_simulator_cmd)
-  ld.add_action(declare_use_sim_time_cmd)
-  ld.add_action(declare_use_simulator_cmd)
-  ld.add_action(declare_world_cmd)
- 
-  # Add any actions
-  ld.add_action(start_gazebo_server_cmd)
-  ld.add_action(start_gazebo_client_cmd)
-  ld.add_action(spawn_entity_cmd)
-  ld.add_action(controller_node)
- 
-  return ld
+    world = os.path.join(get_package_share_directory(
+        robot_name), 'worlds', world_file_name)
+
+    urdf = os.path.join(get_package_share_directory(
+        robot_name), 'urdf', 'gypsy.urdf')
+
+    xml = open(urdf, 'r').read()
+
+    xml = xml.replace('"', '\\"')
+
+    swpan_args = '{name: \"gypsy\", xml: \"' + xml + '\" }'
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', world,
+                 '-s', 'libgazebo_ros_factory.so'],
+            output='screen'),
+
+        ExecuteProcess(
+            cmd=['ros2', 'param', 'set', '/gazebo',
+                 'use_sim_time', use_sim_time],
+            output='screen'),
+
+        ExecuteProcess(
+            cmd=['ros2', 'service', 'call', '/spawn_entity',
+                 'gazebo_msgs/SpawnEntity', swpan_args],
+            output='screen'),
+    ])
